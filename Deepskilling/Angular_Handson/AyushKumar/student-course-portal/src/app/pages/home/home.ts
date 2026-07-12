d623a6c9c75c1c04b895287131edb2cd26980e09
Aayu62 <ayushbarnwal062@gmail.com>
2026-07-12 18:53:49 +0530

    Angular Hands-on 2(Task 16&17)

--- Deepskilling/Angular_Handson/AyushKumar/student-course-portal/src/app/pages/home/home.ts
+++ Deepskilling/Angular_Handson/AyushKumar/student-course-portal/src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -7,15 +7,23 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements OnInit, OnDestroy{
 
   portalName = 'Student Course Portal';
-
   isPortalActive = true;
-
   message = '';
-
   searchTerm = '';
+  availableCourses = 0;
+
+  ngOnInit(): void {
+    //Simulating fetching data from a server
+    this.availableCourses = 12;
+    console.log('HomeComponent initialised - course loaded');
+  }
+
+  ngOnDestroy(): void {
+    console.log('HomeComponent destroyed');
+  }
 
   onEnrollClick() {
     this.message = 'Enrollment opened!';
